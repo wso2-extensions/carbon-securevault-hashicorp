ad7122ebedf238c585e871439b0a32ef57fa24bb
Kavinda Rajapakse <kavindasr@gmail.com>
2025-11-03 10:36:09 +0530

    Revert "Remove log"
    This reverts commit 98feec3f035dd88779fa3562a3825cf872a44fa0.

--- src/main/java/org/wso2/carbon/securevault/hashicorp/repository/HashiCorpSecretRepository.java
+++ src/main/java/org/wso2/carbon/securevault/hashicorp/repository/HashiCorpSecretRepository.java
@@ -101,6 +101,8 @@ public class HashiCorpSecretRepository implements SecretRepository {
     @Override
     public void init(Properties properties, String id) {
 
+        LOG.info("Initializing HashiCorp Secure Vault");
+
         // Load Configurations
         HashiCorpVaultConfigLoader hashiCorpVaultConfigLoader = HashiCorpVaultConfigLoader.getInstance();
         try {
